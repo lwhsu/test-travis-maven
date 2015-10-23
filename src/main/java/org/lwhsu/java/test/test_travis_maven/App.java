e7978ecbaa9024362583abef71b2b395929f5577
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-10-23 17:25:10 +0800

    Init

---
+++ src/main/java/org/lwhsu/java/test/test_travis_maven/App.java
@@ -0,0 +1,13 @@
+package org.lwhsu.java.test.test_travis_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
